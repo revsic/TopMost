128c2ef89b900be2ce6d20f67a15640be1c41bbc
revsic <revsic99@gmail.com>
2019-07-11 04:38:35 +0900

    test.js: Add press any key to continue

--- node-gyp/test.js
+++ node-gyp/test.js
@@ -1,3 +1,7 @@
 var addon = require('bindings')('topmost')
 
-var topper = new addon.TopMostWrapper();
+var topper = new addon.TopMostWrapper(true, 17624);
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
+process.stdin.on('data', process.exit.bind(process, 0));
