7989e7bc9603878072565d89817b6c68038bc1b0
revsic <revsic99@gmail.com>
2019-07-11 05:13:47 +0900

    topmost, node-gyp: Add annotation

--- node-gyp/topmost.cc
+++ node-gyp/topmost.cc
@@ -2,14 +2,22 @@
 
 #include "../src/TopMost.hpp"
 
+//!
+//! \brief TopMostWrapper class.
+//!
+//! This class is javascript wrapper for topmost library.
+//!
 struct TopMostWrapper : public Nan::ObjectWrap {
     std::unique_ptr<TopMost::MakeTop> obj;
 
+    //! Default constructor.
     ~TopMostWrapper() {
         obj.reset();
     }
 
+    //! Module initializer.
     static NAN_MODULE_INIT(Init);
+    //! Module constructor.
     static NAN_METHOD(New);
 
     static Nan::Persistent<v8::FunctionTemplate> constructor;    
@@ -17,6 +25,7 @@ struct TopMostWrapper : public Nan::ObjectWrap {
 
 Nan::Persistent<v8::FunctionTemplate> TopMostWrapper::constructor;
 
+//! Module Initializer.
 NAN_MODULE_INIT(TopMostWrapper::Init) {
     v8::Local<v8::FunctionTemplate> ctor = Nan::New<v8::FunctionTemplate>(TopMostWrapper::New);
     constructor.Reset(ctor);
@@ -27,11 +36,14 @@ NAN_MODULE_INIT(TopMostWrapper::Init) {
     target->Set(Nan::New("TopMostWrapper").ToLocalChecked(), ctor->GetFunction());
 }
 
+//! Module constructor.
 NAN_METHOD(TopMostWrapper::New) {
+    // if constructor is called without new operator
     if (!info.IsConstructCall()) {
         return Nan::ThrowError(Nan::New("TopMostWrapper::New - called without new keyword").ToLocalChecked());
     }
 
+    // if parameter list doesn't match
     if (!(info.Length() == 0
         || (info.Length() == 1 && info[0]->IsBoolean())
         || (info.Length() == 2 && info[0]->IsBoolean() && info[1]->IsNumber())))
@@ -39,16 +51,20 @@ NAN_METHOD(TopMostWrapper::New) {
         return Nan::ThrowError(Nan::New("TopMostWrapper::New - expected arguments: () or (log) or (log, pid)").ToLocalChecked());
     }
 
+    // make new topmost wrapper
     TopMostWrapper* vec = new TopMostWrapper();
     vec->Wrap(info.Holder());
 
+    // parsing parameters
     bool log = info.Length() > 0 ? info[0]->BooleanValue() : false;
     DWORD dwPid = info.Length() > 1 ? info[1]->NumberValue() : GetCurrentProcessId();
 
+    // make TopMost::MakeTop
     vec->obj = std::make_unique<TopMost::MakeTop>(dwPid, true, false, log);
     info.GetReturnValue().Set(info.Holder());
 }
 
+// Module initializer
 NAN_MODULE_INIT(InitModule) {
     TopMostWrapper::Init(target);
 }
